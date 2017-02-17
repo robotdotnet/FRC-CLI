c3e50b952dbdd6eab903f8d5a80a695cb823a1b3
Thad House <thadhouse1@gmail.com>
2017-02-16 19:42:49 -0800

    Abstracts out chunks of NativeDeploymentProvider, and stores as JSON instead of custom

--- src/FRC.CLI.Common/Implementations/NativeContentDeploymentProvider.cs
+++ src/FRC.CLI.Common/Implementations/NativeContentDeploymentProvider.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using FRC.CLI.Base.Enums;
 using FRC.CLI.Base.Interfaces;
+using Newtonsoft.Json;
 
 namespace FRC.CLI.Common.Implementations
 {
@@ -27,75 +29,80 @@ namespace FRC.CLI.Common.Implementations
             m_fileDeployerProvider = fileDeployerProvider;
             m_outputWriter = outputWriter;
         }
+        
+        public IEnumerable<(string file, string hash)> GetFilesToUpdate(Stream inputFileStream, IEnumerable<(string file, string hash)> localFiles)
+        {
+            return GetFilesToUpdate(ReadFilesFromStream(inputFileStream), localFiles);
+        }
+
+        public IEnumerable<(string file, string hash)> GetFilesToUpdate(IEnumerable<(string file, string hash)> remoteFiles, 
+            IEnumerable<(string file, string hash)> localFiles)
+        {
+            return localFiles.Where(x => !remoteFiles.Any(z => x.file == z.file && x.hash == z.hash));
+        }
+
+        public virtual IEnumerable<(string file, string hash)> ReadFilesFromStream(Stream inputFileStream)
+        {
+            using (StreamReader reader = new StreamReader(inputFileStream))
+            using (JsonTextReader jsonReader = new JsonTextReader(reader))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                var retVal = serializer.Deserialize<List<(string file, string hash)>>(jsonReader);
+                if (retVal == null)
+                {
+                    return new List<(string, string)>();
+                }
+                return retVal;
+            }
+        }
+
+        
 
         public async Task DeployNativeContentAsync()
         {
             await m_outputWriter.WriteLineAsync("Deploying native dependencies").ConfigureAwait(false);
-            MemoryStream memStream = new MemoryStream();
-            bool readFile = await m_fileDeployerProvider.ReceiveFileAsync(
-                $"{m_wpilibNativeDeploySettingsProvider.NativeDeployLocation}/{m_wpilibNativeDeploySettingsProvider.NativePropertiesFileName}",
-                memStream, ConnectionUser.Admin).ConfigureAwait(false);
-            
+
             string buildPath = await m_projectInformationProvider.GetProjectBuildDirectoryAsync().ConfigureAwait(false);
             string nativeLoc = Path.Combine(buildPath, NativeDirectory);
-
             var fileMd5List = await GetMd5ForFilesAsync(nativeLoc, m_wpilibNativeDeploySettingsProvider.NativeIgnoreFiles);
 
             if (fileMd5List == null)
             {
                 throw m_exceptionThrowerProvider.ThrowException("Failed to find all files to deploy");
             }
+
+            MemoryStream memStream = new MemoryStream();
+            bool readFile = await m_fileDeployerProvider.ReceiveFileAsync(
+                $"{m_wpilibNativeDeploySettingsProvider.NativeDeployLocation}/{m_wpilibNativeDeploySettingsProvider.NativePropertiesFileName}",
+                memStream, ConnectionUser.Admin).ConfigureAwait(false);
+
             if (!readFile)
             {
+                // TODO: Add Verbose
                 await DeployNativeLibrariesAsync(fileMd5List).ConfigureAwait(false);
                 await m_outputWriter.WriteLineAsync("Successfully deployed native files").ConfigureAwait(false);
                 return;
             }
 
-            memStream.Position = 0;
+            var updateList = GetFilesToUpdate(memStream, fileMd5List).ToList();
 
-            bool foundError = false;
-            int readCount = 0;
-            using (StreamReader reader = new StreamReader(memStream))
+            if (updateList.Count == 0)
             {
-                string line = null;
-                while (!string.IsNullOrWhiteSpace(line = reader.ReadLine()))
-                {
-                    // Split line at =
-                    string[] split = line.Split('=');
-                    if (split.Length < 2) continue;
-                    readCount++;
-                    foreach (Tuple<string, string> tuple in fileMd5List)
-                    {
-                        if (split[0] == tuple.Item1)
-                        {
-                            // Found a match file name
-                            if (split[1] != tuple.Item2)
-                            {
-                                foundError = true;
-                            }
-                            break;
-                        }
-                    }
-                    if (foundError) break;
-                }
+                await m_outputWriter.WriteLineAsync("Native libraries already exist. Skipping").ConfigureAwait(false);
+                return;
             }
 
-            if (foundError || readCount != fileMd5List.Count)
-            {
-                await DeployNativeLibrariesAsync(fileMd5List).ConfigureAwait(false);
-                await m_outputWriter.WriteLineAsync("Successfully deployed native files").ConfigureAwait(false);
-            }
-
-            await m_outputWriter.WriteLineAsync("Native libraries already exist. Skipping").ConfigureAwait(false);
+            await DeployNativeLibrariesAsync(fileMd5List).ConfigureAwait(false);
+            await m_outputWriter.WriteLineAsync("Successfully deployed native files").ConfigureAwait(false);            
         }
 
-        public async Task<List<Tuple<string, string>>> GetMd5ForFilesAsync(string fileLocation, IList<string> ignoreFiles)
+        public virtual async Task<List<(string file, string hash)>> GetMd5ForFilesAsync(string fileLocation, 
+            IEnumerable<string> ignoreFiles)
         {
             if (Directory.Exists(fileLocation))
             {
                 string[] files = Directory.GetFiles(fileLocation);
-                List<Tuple<string, string>> retList = new List<Tuple<string, string>>(files.Length);
+                List<(string file, string hash)> retList = new List<(string file, string hash)>(files.Length);
                 foreach (var file in files)
                 {
                     bool skip = false;
@@ -109,7 +116,7 @@ namespace FRC.CLI.Common.Implementations
                     }
                     if (skip) continue;
                     string md5 = await MD5Helper.Md5SumAsync(file);
-                    retList.Add(new Tuple<string, string>(file, md5));
+                    retList.Add((file, md5));
                 }
                 return retList;
             }
@@ -119,28 +126,19 @@ namespace FRC.CLI.Common.Implementations
             }
         }
 
-        public async Task DeployNativeLibrariesAsync(List<Tuple<string, string>> files)
+        public async Task DeployNativeLibrariesAsync(IList<(string file, string hash)> files)
         {
-            List<string> fileList = new List<string>(files.Count);
             bool nativeDeploy = false;
 
             string tempFile = Path.Combine(Path.GetTempPath(), m_wpilibNativeDeploySettingsProvider.NativePropertiesFileName);
 
-            using (FileStream memStream = new FileStream(tempFile, FileMode.Create))
-            using (StreamWriter writer = new StreamWriter(memStream))
+            await Task.Run(() => 
             {
-                foreach (Tuple<string, string> tuple in files)
-                {
-                    fileList.Add(tuple.Item1);
-                    await writer.WriteLineAsync($"{tuple.Item1}={tuple.Item2}").ConfigureAwait(false);
-                }
+                var json = JsonConvert.SerializeObject(files, Formatting.Indented);
+                File.WriteAllText(tempFile, json);
+            });
 
-                writer.Flush();
-
-                fileList.Add(tempFile);
-            }
-
-            nativeDeploy = await m_fileDeployerProvider.DeployFilesAsync(fileList, 
+            nativeDeploy = await m_fileDeployerProvider.DeployFilesAsync(files.Select(x => x.file), 
                 m_wpilibNativeDeploySettingsProvider.NativeDeployLocation, ConnectionUser.Admin).ConfigureAwait(false);
             // TODO: Figure out why trying to deploy the raw MemoryStream was Deadlocking
             //md5Deploy = await rioConn.DeployFileAsync(memStream, $"{remoteDirectory}/{propertiesName}32.properties", ConnectionUser.Admin).ConfigureAwait(false);
