6adf2421acb7f1bd4bf607f94954c5605f648730
Thad House <thadhouse1@gmail.com>
2017-05-26 15:42:12 -0700

    Moves FRCCommand to its own file

--- src/dotnet-frc/Program.cs
+++ src/dotnet-frc/Program.cs
@@ -1,36 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using dotnet_frc.Commands;
-using Microsoft.DotNet.Cli;
-using Microsoft.DotNet.Cli.Utils;
+﻿using Microsoft.DotNet.Cli.Utils;
 
 
 namespace dotnet_frc
 {
-    public class FrcCommand : DotNetTopLevelCommandBase
-    {
-        protected override string CommandName => "frc";
-        protected override string FullCommandNameLocalized => ".NET FRC Utility";
-        protected override string ArgumentName => Constants.ProjectArgumentName;
-        protected override string ArgumentDescriptionLocalized =>
-            "The project file to operate on. If a file is not specified," +
-            " the command will search the current directory for one.";
-        internal override List<Func<DotNetSubCommandBase>> SubCommands =>
-            new List<Func<DotNetSubCommandBase>>
-            {
-                DeployCommand.Create,
-                KillCommand.Create,
-                SettingsCommand.Create,
-                RuntimeCommand.Create
-            };
-
-        public static int Run(string[] args)
-        {
-            var command = new FrcCommand();
-            return command.RunCommand(args);
-        }
-    }
-
     internal class Program
     {
         private static int Main(string[] args)
